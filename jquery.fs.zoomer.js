7de451b9bde30d70eb8c96e02e544ca24d0fa19d
Ben Plum <mr@benplum.com>
2013-09-30 18:22:54 -0400

    Removing polyfill, still required just not distributed by me. Added link to docs, available here: https://gist.github.com/paulirish/1579671

--- jquery.fs.zoomer.js
+++ jquery.fs.zoomer.js
@@ -1,7 +1,7 @@
 /*
  * Zoomer [Formstone Library]
  * @author Ben Plum
- * @version 0.3.1
+ * @version 0.3.2
  *
  * Copyright © 2013 Ben Plum <mr@benplum.com>
  * Released under the MIT License <http://www.opensource.org/licenses/mit-license.php>
@@ -969,31 +969,3 @@ if (jQuery) (function($) {
 		return this;	
 	};
 })(jQuery);
-
-
-// Request Animation Frame Polyfill 
-// Paul Irish <https://gist.github.com/paulirish/1579671>
-(function() {
-	var lastTime = 0;
-	var vendors = ['webkit', 'moz'];
-	for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-		window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-		window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
-	}
-	
-	if (!window.requestAnimationFrame) {
-		window.requestAnimationFrame = function(callback, element) {
-			var currTime = new Date().getTime();
-			var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-			var id = window.setTimeout(function() { callback(currTime + timeToCall); }, timeToCall);
-			lastTime = currTime + timeToCall;
-			return id;
-		};
-	}
-	
-	if (!window.cancelAnimationFrame) {
-		window.cancelAnimationFrame = function(id) {
-			clearTimeout(id);
-		};
-	}
-}());
